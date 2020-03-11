062a12d72e41f678ed7a371d568e85211c4339a8
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-03-11 11:37:25 -0400

    TA#19904 Add ui_color_konvergo (#244)

---
+++ konvergo/ui_color_konvergo/README.md
@@ -0,0 +1,13 @@
+# Red Odoo Community Interface
+
+This module changes the web ui color to red .
+
+![App Switcher](static/description/apps.png?raw=true)
+
+![Form View](static/description/form.png?raw=true)
+
+## Contributors
+  * Numigi (tm) and all its contributors (<https://bit.ly/numigiens>)
+
+## More information
+  * Meet us at <https://bit.ly/numigi-com>
---
+++ konvergo/ui_color_konvergo/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Odoo Community Interface for Konvergo',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'Render the Odoo Community interface for Konvergo',
+    'depends': ['muk_res_config_settings_hidden'],
+    'data': ['views/ui_color.xml'],
+    'installable': True,
+}
