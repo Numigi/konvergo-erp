d833e72dd0452e58e93aa4f8c3cdf6b78e5efc3a
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-01 14:21:14 -0400

    TA#23226 Fix konvergo login page (#261)
    * Scope css of konvergo login page
    
    Otherwise, the whole website is impacted.
    
    * Integrate konvergo login page with website

---
+++ konvergo/konvergo_login_page_website/README.rst
@@ -0,0 +1,11 @@
+Konvergo Login Page / Website
+=============================
+This module prevents the website module from breaking the Konvergo login page.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ konvergo/konvergo_login_page_website/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2020 - Today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Konvergo Login Page / Website",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Binding between Konvergo login page and website",
+    "depends": ["konvergo_login_page", "website"],
+    "data": ["views/login_page.xml",],
+    "installable": True,
+    "auto_install": True,
+}
