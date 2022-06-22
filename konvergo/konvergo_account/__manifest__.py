1c7a3249d28774572157f74bcbd62cf8e89ed38c
Julien <104652274+jjbreard2@users.noreply.github.com>
2022-06-22 16:53:48 -0400

    Ta#45128   restore mis builder cash flow (#558)
    * TA#45128 - restore mis_builder_cash_flow
    
    * TA#45128 - update konvergo_account
    
    Co-authored-by: Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>

--- konvergo/konvergo_account/__manifest__.py
+++ konvergo/konvergo_account/__manifest__.py
@@ -40,7 +40,7 @@
         # OCA/account-financial-reporting
         # 'account_export_csv',  # TA#16549
         'account_financial_report',  # TA#16549
-        # 'mis_builder_cash_flow',  # TA#16549
+        'mis_builder_cash_flow',  # TA#45128
         'partner_statement',  # TA#45126
 
         # OCA/account-financial-tools
