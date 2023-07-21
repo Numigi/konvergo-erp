96cd4c933cf611684a31da63077c7d3d15399b69
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-07-21 18:50:44 +0300

    TA#25792 [ADD] konvergo_web_logo : add module (#775)
    * TA#25792 [ADD] konvergo_web_logo : add module
    
    ---------
    
    Co-authored-by: Majda EL MARIOULI <majdaelmariouli@gmail.com>

---
+++ konvergo/konvergo_web_logo/README.rst
@@ -0,0 +1,13 @@
+Konvergo Web Logo
+=================
+This module set Konvergo logo for the database manager webpage.
+
+.. image:: static/src/img/logo-konvergo.png
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ konvergo/konvergo_web_logo/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Konvergo Web Logo",
+    "version": "1.1.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "category": "Other",
+    "summary": "Changes the logo on the database manager webpage into Konvergo logo.",
+    "depends": [
+        "web",
+    ],
+    "installable": True,
+}
