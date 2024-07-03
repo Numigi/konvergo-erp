784b1861dea9b9f65f35d8c8b628a1b3a4794321
majouda <majdaelmariouli@gmail.com>
2024-07-03 14:31:16 -0400

    Code Review

--- konvergo_bot/tests/__init__.py
+++ konvergo_bot/tests/__init__.py
@@ -1,2 +1,5 @@
-# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import test_bot_answers
+from . import test_bot_name
