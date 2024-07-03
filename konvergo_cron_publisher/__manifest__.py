3f9307bf834c85d223c53ea82c95f9bfc12326e0
majouda <majdaelmariouli@gmail.com>
2024-07-03 14:35:52 -0400

    Code Review

--- konvergo_cron_publisher/__manifest__.py
+++ konvergo_cron_publisher/__manifest__.py
@@ -1,4 +1,4 @@
-# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
@@ -10,7 +10,7 @@
     'category': 'Other',
     'summary': 'Disable the cron notifying odoo',
     'depends': [
-        'mail',  # TA#16530
+        'mail',
     ],
     'data': [
         'data/ir_cron_data.xml',
