c1c1a3d2fa0cfc17b3c032800d4c434431e2c921
majouda <majdaelmariouli@gmail.com>
2024-07-03 11:15:13 -0400

    Code Review

--- konvergo_web_logo/__init__.py
+++ konvergo_web_logo/__init__.py
@@ -1,4 +1,4 @@
-# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import controllers
