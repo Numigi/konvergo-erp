6f57b7afa0cef0cb0a55996a696e7af88a46d904
Abdellatif BENZBIRIA <a.benzbiria@magnetomedia.net>
2024-07-03 20:26:22 +0200

    TA#66882  [16.0][MIG] konvergo_favicon_title (#11)
    Co-authored-by: majouda <majdaelmariouli@gmail.com>

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -12,6 +12,7 @@
     "summary": "Install all addons required for testing.",
     "depends": [
         "base",
+        "konvergo_favicon_title",
         "konvergo_icons",
         "konvergo_login_page",
         "konvergo_mail_notification",
