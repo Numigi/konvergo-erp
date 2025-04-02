8cd76ae9ebae09bf7e240597181e81abe17e3fda
Lanto Razafindrabe <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-04-02 15:56:08 +0300

    TA#66494 [14.0][ADD] konvergo_contact (#21)

---
+++ konvergo_contact/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2025-today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Konvergo / Contact",
+    "version": "14.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "category": "Other",
+    "summary": "Contact Dependencies for Konvergo",
+    "depends": [
+        # Numigi/odoo-partner-addons
+        "contacts_config_menu_moved_right",
+        "partner_edit_group",
+        "partner_category_type",
+        "partner_firstname_before_lastname",
+        "partner_full_text_search",
+        "partner_phone_no_envelope",
+        "partner_key_date",
+        # OCA/partner-contact
+        "partner_firstname",
+        "partner_email_duplicate_warn",
+        "partner_identification",
+    ],
+    "data": [],
+    "installable": True,
+}
