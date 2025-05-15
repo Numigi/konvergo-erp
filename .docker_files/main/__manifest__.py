126b9e60d71fdc213ffef0f3f468806aae2479b2
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-05-15 09:07:06 +0300

    TA#66878 [16.0][MIG] konvergo_accoun

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -11,6 +11,7 @@
     "category": "Other",
     "summary": "Install all addons required for testing.",
     "depends": [
+        "konvergo_account",
         "konvergo_bot",
         "konvergo_cron_publisher",
         "konvergo_favicon_title",
