f37aa9fe2283863acfd6f47ea7370e7eb4dda5ad
Quentin Lavallée-Bourdeau <quentin.lavalleeb@gmail.com>
2025-05-27 14:20:56 -0400

    TA#77954 [14.0][ADD] CI/CD files

---
+++ .docker_files/main/__manifest__.py
@@ -0,0 +1,37 @@
+# © 2018 Numigi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Main Module",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Install all addons required for testing.",
+    "depends": [
+        "canada_mis_report",
+        "canada_vat_label",
+        "konvergo_account",
+        "konvergo_account_fr",
+        "konvergo_base",
+        "konvergo_bot",
+        "konvergo_contact",
+        "konvergo_cron_publisher",
+        "konvergo_favicon_title",
+        "konvergo_icons",
+        "konvergo_login_page",
+        "konvergo_login_page_website",
+        "konvergo_mail_notification",
+        "konvergo_mail_templates",
+        "konvergo_product",
+        "konvergo_sale",
+        "konvergo_web_logo",
+        "lang_fr_activated",
+        "mail_color_konvergo",
+        "mail_template_fr_fields",
+        "ui_color_konvergo",
+    ],
+    "installable": True,
+}
