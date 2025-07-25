c5e8f6bebb45d049d1d881d8c2ce2bae23cfc866
Quentin Lavallée-Bourdeau <quentin.lavalleeb@gmail.com>
2025-07-25 16:39:22 -0400

    test konvergo_account without konvergo_account_fr

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -13,8 +13,8 @@
     "depends": [
         "canada_mis_report",
         "canada_vat_label",
-        # "konvergo_account",
-        "konvergo_account_fr",
+        "konvergo_account",
+        # "konvergo_account_fr",
         "konvergo_base",
         "konvergo_bot",
         # "konvergo_contact",
