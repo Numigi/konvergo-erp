a34a66c734f63e63c95a2536424defe0918b3330
Quentin Lavallée-Bourdeau <quentin.lavalleeb@gmail.com>
2026-03-31 17:11:57 -0400

    TA#83759 [18.0][REN] Rename product/pos packs and remove obsolete branding modules
    Rename modules to pack naming convention:
    - konvergo_product -> konvergo_pack_product
    - konvergo_pos -> konvergo_pack_pos
    
    Update manifests to v18:
    - Update version to 18.0.1.0.0
    - Comment out all dependencies for gradual migration
    - Add standard Odoo apps as base dependencies (product, point_of_sale)
    
    Remove obsolete branding modules:
    - ui_color_konvergo (functionality moved to konvergo_brand)
    - konvergo_icons (functionality moved to konvergo_brand)
    
    Update Dockerfile and main module to reflect changes.

---
+++ konvergo_pack_pos/__manifest__.py
@@ -0,0 +1,37 @@
+# © 2025 Numigi (tm) and all its contributors (https://numigi.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Konvergo Pack - Point of Sale",
+    "version": "18.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://numigi.com",
+    "license": "LGPL-3",
+    "category": "Point of Sale",
+    "summary": "Point of Sale pack for Konvergo",
+    "depends": [
+        "point_of_sale",
+        # OCA/pos - Dépendances à activer
+        # "pos_edit_order_line",
+        # "pos_order_remove_line",
+        # "pos_order_product_search",
+        # "pos_product_sort",
+        # "pos_show_clock",
+        # "pos_show_config_name",
+        # "pos_supplierinfo_search",
+        # "pos_warning_exiting",
+        # "pos_payment_change",
+        # "pos_order_mgmt",
+        # "pos_receipt_hide_price",
+        # "pos_require_product_quantity",
+        # "pos_partner_birthdate",
+        # "pos_report_discount",
+        # "pos_timeout",
+        # "pos_empty_home",
+        # "pos_access_right",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
