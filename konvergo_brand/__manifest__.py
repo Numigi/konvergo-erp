515519eb41e7c43e4f047e07e92629e9702fbb56
Quentin Lavallée-Bourdeau <quentin.lavalleeb@gmail.com>
2026-03-31 16:54:49 -0400

    TA#83759 [18.0][ADD] New architecture - Technical layer
    Create new meta-modules for technical and branding layers:
    - konvergo_core: Technical foundations and security
    - konvergo_ui: UI/UX improvements (white-label approach)
    - konvergo_brand: Konvergo visual identity
    
    All OCA/Numigi dependencies are commented out for gradual migration.

---
+++ konvergo_brand/__manifest__.py
@@ -0,0 +1,26 @@
+# © 2025 Numigi (tm) and all its contributors (https://numigi.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Konvergo Brand",
+    "version": "18.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://numigi.com",
+    "license": "LGPL-3",
+    "category": "Branding",
+    "summary": "Konvergo visual identity (colors, logos, login page)",
+    "depends": [
+        "web",
+        # Modules de branding Konvergo à activer
+        # "konvergo_web_logo",
+        # "konvergo_favicon_title",
+        # "konvergo_icons",
+        # "konvergo_login_page",
+        # "ui_color_konvergo",
+
+        # OCA/web - Theme backend
+        # "muk_web_theme",
+    ],
+    "installable": True,
+}
---
+++ konvergo_core/__manifest__.py
@@ -0,0 +1,31 @@
+# © 2025 Numigi (tm) and all its contributors (https://numigi.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Konvergo Core",
+    "version": "18.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://numigi.com",
+    "license": "LGPL-3",
+    "category": "Technical",
+    "summary": "Technical foundations and security for Konvergo",
+    "depends": [
+        "base",
+        # OCA/server-tools - Dépendances techniques à activer
+        # "module_change_auto_install",
+        # "web_session_auto_close",
+        # "web_disable_export_group",
+        # "base_optional_quick_create",
+
+        # OCA/server-brand
+        # "disable_odoo_online",
+        # "remove_odoo_enterprise",
+
+        # Numigi/odoo-base-addons
+        # "mail_bot_no_pong",
+        # "mail_notification_no_action_button",
+        # "base_extended_security",
+    ],
+    "installable": True,
+}
